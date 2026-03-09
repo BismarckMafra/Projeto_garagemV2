465204080e95cac638d133cf3511434b86e49299
Bismarck <bismarck_mafra@estudante.sesisenai.org.br>
2026-03-09 20:07:19 -0300

    alinhando routes

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -5,64 +5,67 @@ const { saveUser, readUsers } = require('../utils/fileHandler');
 
 
 router.get('/listar_carro', (req, res) => {
-    const usuarios = readUsers();
-    res.json(usuarios)
+    const carros = readUsers();
+    res.json(carros)
 })
 
 router.get('/listar_carro/:id', (req, res) => {
     const id_carros = parseInt(req.params.id);
     const lista_carros = readUsers();
-    const usuario = lista_carros.find(u => u.id === id_carros);
-    if (usuario) {
-        res.json(usuario);
+    const carros = lista_carros.find(u => u.id === id_carros);
+    if (carros) {
+        res.json(carros);
     } else {
         res.status(404).json({ message: "Carro não encontrado" });
     }
 })
 
 router.post('/cadastrar_carros', (req, res) => {
-    const lista_usuarios = readUsers();
-    const novo_id = lista_usuarios.length > 0 ? Math.max(...lista_usuarios.map(u => u.id + 1))  : 1
+    const lista_carros = readUsers();
+    const novo_id = lista_carros.length > 0 ? Math.max(...lista_carros.map(u => u.id + 1))  : 1
  
-    const novo_usuario = {
+    const carro = {
         id: novo_id,
-        nome: req.body.nome,
-        marca: req.body.marca,
-        modelo: req.body.modelo,
-        ano: req.body.ano,
+        categoria: req.body.categoria,
+        marca_car: req.body.marca_car,
+        modelo_car: req.body.modelo_car,
+        ano_modelo: req.body.ano_modelo,
         cor: req.body.cor
     }
 
-    lista_usuarios.push(novo_usuario);
-    saveUser(lista_usuarios);
-    res.status(201).json(novo_usuario);
+    lista_carros.push(carro);
+    saveUser(lista_carros);
+    res.status(201).json(carro);
 })
 
 router.put('/atualizar_carros/:id', (req, res) => {
     const id_carros = parseInt(req.params.id);
-    const lista_usuarios = readUsers();
-    const usuarioIndex = lista_usuarios.findIndex(u => u.id === id_usuarios)
+    const lista_carros = readUsers();
+    const carroIndex = lista_carros.findIndex(u => u.id === id_carros);
 
-    if (usuarioIndex !== -1) {
-        lista_usuarios[usuarioIndex].nome = req.body.nome || lista_usuarios[usuarioIndex].nome;
-        lista_usuarios[usuarioIndex].sobrenome = req.body.sobrenome || lista_usuarios[usuarioIndex].sobrenome;
-        saveUser(lista_usuarios);
-        res.json(lista_usuarios[usuarioIndex]);
+    if (carroIndex !== -1) {
+        lista_carros[carroIndex].categoria = req.body.categoria || lista_carros[carroIndex].categoria;
+        lista_carros[carroIndex].marca_car = req.body.marca_car || lista_carros[carroIndex].marca_car;
+        lista_carros[carroIndex].modelo_car = req.body.modelo_car || lista_carros[carroIndex].modelo_car;
+        lista_carros[carroIndex].ano_modelo = req.body.ano_modelo || lista_carros[carroIndex].ano_modelo;
+        lista_carros[carroIndex].cor = req.body.cor || lista_carros[carroIndex].cor;
+        saveUser(lista_carros);
+        res.json(lista_carros[carroIndex]);
     } else {
-        res.status(404).json({ message: "Usuário não encontrado" });
+        res.status(404).json({ message: "Carro não encontrado" });
     }
 })
 
-router.delete('/deletar_usuarios/:id', (req, res) => {
-    const id_usuarios = parseInt(req.params.id);
-    const lista_usuarios = readUsers();
-    const usuarioIndex = lista_usuarios.findIndex(u => u.id === id_usuarios)
-    if (usuarioIndex !== -1) {
-        lista_usuarios.splice(usuarioIndex, 1);
-        saveUser(lista_usuarios);
-        res.json({ message: "Usuário deletado com sucesso" });
+router.delete('/deletar_carros/:id', (req, res) => {
+    const id_carros = parseInt(req.params.id);
+    const lista_carros = readUsers();
+    const carroIndex = lista_carros.findIndex(u => u.id === id_carros)
+    if (carroIndex !== -1) {
+        lista_carros.splice(carroIndex, 1);
+        saveUser(lista_carros);
+        res.json({ message: "Carro deletado com sucesso" });
     } else {
-        res.status(404).json({ message: "Usuário não encontrado" });
+        res.status(404).json({ message: "Carro não encontrado" });
     }
 })
 
