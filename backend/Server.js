5693fd18f3fb619b9f39bc2c4cdb27ca805cdf7b
Bismarck <bismarck_mafra@estudante.sesisenai.org.br>
2026-03-09 19:24:29 -0300

    estrutura backend modificada

---
+++ backend/Server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+const userRoutes = require('./routes/users.routes');
+
+
+
+app.use(express.json());
+app.use(cors({origin:'http://localhost:3001'}));
+app.use('/usuarios', userRoutes);
+
+app.listen(port, () => {
+    console.log(`Server rodando em http://localhost:${port}`)
+});
+
+
+app.get('/', (req, res) => {
+    console.log(`vc acessou a rota /`);
+    res.json("Bem vindo a API de usuários");
+
+})
---
+++ backend/utils/filehandler.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+
+const dataPath = path.join(__dirname, '../data/users.json');
+
+function readUsers(){
+    try{
+        const data = fs.readFileSync(dataPath, 'utf8')
+         return JSON.parse(data);
+    }catch(err){
+        return []
+    }   
+}
+
+function saveUser(users){
+    fs.writeFileSync(dataPath, JSON.stringify(users, null, 2));
+}
+
+module.exports = {
+    saveUser, readUsers
+}
