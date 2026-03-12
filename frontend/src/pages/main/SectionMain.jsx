65bcabfaceced0d7b5a4b6c9623e12d354801ff1
FabioJuniorJR <fjrcorreoli@gmail.com>
2026-03-12 20:59:12 -0300

    ataualizando front

---
+++ frontend/src/pages/main/SectionMain.jsx
@@ -0,0 +1,15 @@
+import "./style.jsx"
+
+    function SectionMain() {
+        return (
+            <section>
+                <p>"Louco por carros"</p>
+                <p>"Apaixonados por velocidade"</p>
+                <p>"Amantes de Hot Wheels"</p>
+
+                <p>"Trazemos um pouco de tecnologia junto da velocidade. E para isso se tornar possivel, colocamos neste site um pedaço da gigantesca marca da hotWheels."</p>
+            </section>
+        )
+    }
+
+export default SectionMain
