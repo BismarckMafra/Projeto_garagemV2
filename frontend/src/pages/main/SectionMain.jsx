db8fd71cdf83ad1225eca763264b2e0f0232bf89
FabioJuniorJR <fjrcorreoli@gmail.com>
2026-03-12 21:01:10 -0300

    atualizando front

--- frontend/src/pages/main/SectionMain.jsx
+++ frontend/src/pages/main/SectionMain.jsx
@@ -1,15 +1,14 @@
 import "./style.jsx"
 
-    function SectionMain() {
-        return (
-            <section>
-                <p>"Louco por carros"</p>
-                <p>"Apaixonados por velocidade"</p>
-                <p>"Amantes de Hot Wheels"</p>
-
-                <p>"Trazemos um pouco de tecnologia junto da velocidade. E para isso se tornar possivel, colocamos neste site um pedaço da gigantesca marca da hotWheels."</p>
-            </section>
-        )
-    }
+function SectionMain() {
+    return (
+        <section>
+            <p>"Louco por carros"</p>
+            <p>"Apaixonados por velocidade"</p>
+            <p>"Amantes de Hot Wheels"</p>
+            <p>"Trazemos um pouco de tecnologia junto da velocidade. E para isso se tornar possivel, colocamos neste site um pedaço da gigantesca marca da hotWheels."</p>
+        </section>
+    )
+}
 
 export default SectionMain
